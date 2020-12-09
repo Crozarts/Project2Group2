d3fd9ce7132b544b68303f8668ee91879fc1546b
S Galarza <sgalarza4190@gmail.com>
2020-12-09 14:16:22 -0500

    installed sequelize

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,24 @@
-// Dependencies
-const express = require("express");
+// // Dependencies
+// const express = require("express");
 
-var app = express();
+// var app = express();
 
-app.get("/", function(req,res){
-    res.send("../public/index");
-});
+// app.get("/", function (req, res) {
+//     res.send("../public/index");
+// });
 
-app.get("/market", function(req,res){
-    res.send("../public/market");
-});
+// app.get("/market", function (req, res) {
+//     res.send("../public/market");
+// });
 
-app.get("/myStore", function(req,res){
-    res.send("../public/myStore");
-});
+// app.get("/home", function (req, res) {
+//     res.send("../public/home");
+// });
 
-app.get("/user", function(req,res){
-    res.send("../public/user");
-});
+// app.get("/user", function (req, res) {
+//     res.send("../public/user");
+// });
+
+// app.get("/activesales", function (req, res) {
+//     res.send("../public/activesales");
+// });
