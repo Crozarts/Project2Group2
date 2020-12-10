41faf095b27f5905638d3ab6074637a264a3dce1
S Galarza <sgalarza4190@gmail.com>
2020-12-10 13:03:53 -0500

    fixed sequelize model require to be function with versions greater than 6.0.0

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ require("./routes/htmlRoutes.js")(app);
 require("./routes/apiRoutes.js")(app);
 
 // Syncing our sequelize model and then starting our Express app
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync().then(function() {
     app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT);
     });
