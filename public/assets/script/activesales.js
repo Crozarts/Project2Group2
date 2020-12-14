f6994c171c5b990399fc641cee3cd79cda820b3d
Dimitar Micevski <dmicevski@hotmail.com>
2020-12-14 14:04:48 -0500

    updated activesales js and html files

--- public/assets/script/activesales.js
+++ public/assets/script/activesales.js
@@ -6,12 +6,12 @@ $(document).ready(function () {
     // Our new todos will go inside the productContainer
     var $productContainer = $(".product-container");
     // Adding event listeners for deleting, editing, and adding products
-    $(document).on("click", "button.delete", deleteProduct);
-    $(document).on("click", "button.sell", toggleSell);
-    $(document).on("click", "button.trade", toggleTrade);
-    $(document).on("click", ".product-item", editProduct);
-    $(document).on("keyup", ".product-item", finishEdit);
-    $(document).on("blur", "button.delete", cancelEdit);
+    // $(document).on("click", "button.delete", deleteProduct);
+    $(document).on("click", "input.sell", toggleSell);
+    $(document).on("click", "input.trade", toggleTrade);
+    // $(document).on("click", ".product-item", editProduct);
+    // $(document).on("keyup", ".product-item", finishEdit);
+    // $(document).on("blur", "button.delete", cancelEdit);
     $(document).on("submit", "#product-form", insertProduct);
 
     // Our Initial product array
@@ -39,23 +39,23 @@ $(document).ready(function () {
     }
 
     // This function deletes a product when the user clicks the delete button
-    function deleteProduct(event) {
-        event.stopPropogation();
-        var id = $(this).data("id");
-        $.ajax({
-            method: "DELETE",
-            url: "/api/todos" + id
-        }).then(getProducts);
-    }
+    // function deleteProduct(event) {
+    //     event.stopPropogation();
+    //     var id = $(this).data("id");
+    //     $.ajax({
+    //         method: "DELETE",
+    //         url: "/api/todos" + id
+    //     }).then(getProducts);
+    // }
 
     // This function handles showing the input box for a user to edit their product
-    function editProduct() {
-        var currentProduct = $(this).data("product");
-        $(this).children().hide();
-        $(this).children("input.edit").val(currentProduct.text);
-        $(this).children("input.edit").show();
-        $(this).children("input.edit").focus();
-    }
+    // function editProduct() {
+    //     var currentProduct = $(this).data("product");
+    //     $(this).children().hide();
+    //     $(this).children("input.edit").val(currentProduct.text);
+    //     $(this).children("input.edit").show();
+    //     $(this).children("input.edit").focus();
+    // }
 
     // Toggles sell status
     function toggleSell(event) {
@@ -75,36 +75,36 @@ $(document).ready(function () {
 
     // This function starts updating a product in the database if a user hits the "Enter Key"
     // While in edit mode
-    function finishEdit(event) {
-        var updatedProduct = $(this).data("product");
-        if (event.which === 13) {
-            updatedProduct.text = $(this).children("input").val().trim();
-            $(this).blur();
-            updateProduct(updatedProduct);
-        }
-    }
+    // function finishEdit(event) {
+    //     var updatedProduct = $(this).data("product");
+    //     if (event.which === 13) {
+    //         updatedProduct.text = $(this).children("input").val().trim();
+    //         $(this).blur();
+    //         updateProduct(updatedProduct);
+    //     }
+    // }
 
     // This function updates a product in our database
-    function updateProduct(product) {
-        $.ajax({
-            method: "PUT",
-            url: "/api/products",
-            data: product
-        }).then(getProducts);
-    }
+    // function updateProduct(product) {
+    //     $.ajax({
+    //         method: "PUT",
+    //         url: "/api/products",
+    //         data: product
+    //     }).then(getProducts);
+    // }
 
     // The function is called whenver a product item is in edit mode and loses focus
     // This cancels any edits being made
 
-    function cancelEdit() {
-        var currentProduct = $(this).data("product");
-        if (currentProduct) {
-            $(this).children().hide();
-            $(this).children("input.edit").val(currentProduct.text);
-            $(this).children("span").show();
-            $(this).children("button").show();
-        }
-    }
+    // function cancelEdit() {
+    //     var currentProduct = $(this).data("product");
+    //     if (currentProduct) {
+    //         $(this).children().hide();
+    //         $(this).children("input.edit").val(currentProduct.text);
+    //         $(this).children("span").show();
+    //         $(this).children("button").show();
+    //     }
+    // }
 
     // This function inserts a new product into our database and then updates the view
     function insertProduct(event) {
